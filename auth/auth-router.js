44cae4df0a04c08049f0d8a498295b1c3f97e1d7
Lynda Santiago <lyntechi@gmail.com>
2020-09-15 18:51:38 -0400

    created knexfile and dbconfig

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,7 @@
-const bc = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const router = require("express").Router();
 
 router.post("/register", (req, res) => {});
 
 router.post("/login", (req, res) => {});
 
-router.get("/");
